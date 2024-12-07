55a719f7e027e04464b47770ec28cc1ebcd42d77
reza_moein <rezamoeinpe@gmail.com>
2024-12-07 06:55:41 +0330

    Close #1

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,87 @@
-use std::convert::Infallible;
-use std::net::SocketAddr;
-
-use http_body_util::Full;
-use hyper::body::Bytes;
-use hyper::server::conn::http1;
-use hyper::service::service_fn;
-use hyper::{Request, Response};
-use hyper_util::rt::TokioIo;
-use tokio::net::TcpListener;
-
-async fn hello(req: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
-    println!("#####Request Headers#### ");
-    for ele in req.headers() {
-        println!("{}:{:?}", ele.0, ele.1);
-    }
-    Ok(Response::new(Full::new(Bytes::from("Hello, World!"))))
-}
-
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    let port = 3000;
-    let addr = SocketAddr::from(([127, 0, 0, 1], port));
+    web_server::WebServer::run().await
+}
 
-    // We create a TcpListener and bind it to 127.0.0.1:3000
-    let listener = TcpListener::bind(addr).await?;
+mod web_server {
+    use std::{convert::Infallible, net::SocketAddr};
 
-    println!("Server start on port {}", port);
-    // We start a loop to continuously accept incoming connections
-    loop {
-        let (stream, _) = listener.accept().await?;
-        // Use an adapter to access something implementing `tokio::io` traits as if they implement
-        // `hyper::rt` IO traits.
-        let io = TokioIo::new(stream);
+    use http_body_util::Full;
+    use hyper::{body::Bytes, server::conn::http1, service::service_fn, Request, Response};
+    use hyper_util::rt::TokioIo;
+    use tokio::net::TcpListener;
 
-        // Spawn a tokio task to serve multiple connections concurrently
-        tokio::task::spawn(async move {
-            // Finally, we bind the incoming connection to our `hello` service
-            if let Err(err) = http1::Builder::new()
-                // `service_fn` converts our function in a `Service`
-                .serve_connection(io, service_fn(hello))
-                .await
-            {
-                eprintln!("Error serving connection: {:?}", err);
+    pub struct WebServer {}
+    impl WebServer {
+        pub async fn run() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+            // TODO must read from configuration
+            let port = 3000;
+            let addr = SocketAddr::from(([127, 0, 0, 1], port));
+            println!("Server started on port {}", port);
+            let listener = TcpListener::bind(addr).await?;
+
+            loop {
+                let (stream, _) = listener.accept().await?;
+
+                let io = TokioIo::new(stream);
+
+                tokio::task::spawn(async move {
+                    if let Err(err) = http1::Builder::new()
+                        .serve_connection(io, service_fn(Self::hello))
+                        .await
+                    {
+                        eprintln!("Error serving connection: {:?}", err);
+                    }
+                });
             }
-        });
+        }
+        async fn hello(
+            _: Request<hyper::body::Incoming>,
+        ) -> Result<Response<Full<Bytes>>, Infallible> {
+            Ok(Response::new(Full::new(Bytes::from("Hello, World!\n"))))
+        }
+    }
+}
+
+mod domain {
+
+    use chrono::{DateTime, Local};
+    struct Bill {
+        order: Order,
+        bill_type: BillType,
+        total_amount: Amount,
+        payors: Vec<Participant>,
+    }
+
+    struct Order {
+        items: Vec<OrderItem>,
+        where_shit_happend: Place,
+        when_shit_happend: DateTime<Local>,
+        participants: Vec<Participant>,
+    }
+
+    struct OrderItem {
+        title: String,
+        item_amount: Amount,
+    }
+
+    enum BillType {
+        FOOD,
+        DRINK,
+        TRIP,
+        OTHER,
+    }
+
+    struct Place {
+        title: String,
+    }
+
+    struct Amount {
+        value: f64,
+        ccy: String,
+    }
+
+    struct Participant {
+        name: String,
     }
 }
